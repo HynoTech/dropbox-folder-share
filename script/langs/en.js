3d22323e01d114b87da93a22be4b7b81d6d27d8d
Antonio Salas <antony.salas@gmail.com>
2019-07-23 17:06:04 -0500

    Web DropboxFolderShare

--- script/langs/en.js
+++
@@ -1,8 +0,0 @@
-tinyMCE.addI18n('en.DropBoxFolderShare',{
-	descripcion:"Dropbox Folder",
-	titulo:"DropBox Folder Share WP",
-	txt_url:"Folder URL",
-	txt_ver_como_lista:"View as List",
-	txt_ver_como_iconos: "View as Icons",
-	txt_necesario: "Required Fields"
-});
--- script/langs/es.js
+++
@@ -1,8 +0,0 @@
-tinyMCE.addI18n('es.DropBoxFolderShare',{
-	descripcion:"Carpeta Dropbox",
-	titulo:"DropBox Folder Share WP",
-	txt_url:"URL de carpeta",
-	txt_ver_como_lista:"Ver como Lista",
-	txt_ver_como_iconos: "Ver solo Iconos",
-	txt_necesario: "Campos Obligatorios"
-});
