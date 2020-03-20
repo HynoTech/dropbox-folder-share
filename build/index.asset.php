d8a67f9551c6dbec4b54f1ba44209dd80d6d12dd
Antony Salas <antony.salas@gmail.com>
2020-03-20 00:23:42 -0500

    rebuild v1.9

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-element', 'wp-polyfill'), 'version' => 'bdd88977603b3f428a0751db7b41bd9a');
+<?php return array('dependencies' => array('wp-element', 'wp-polyfill'), 'version' => '9df63dcc4db67eb1b3eac1e3b39b4a19');
